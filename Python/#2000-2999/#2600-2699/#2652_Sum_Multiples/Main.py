d489e09c7199d308cb2babecc18afc75f1f50adc
Ryan Warrener <ryan.warrener@ontariotechu.net>
2025-09-18 12:04:10 -0400

    Completed #2652

---
+++ Python/#2000-2999/#2600-2699/#2652_Sum_Multiples/Main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sumOfMultiples(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        nums = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                nums.append(i)
+        return sum(nums)
