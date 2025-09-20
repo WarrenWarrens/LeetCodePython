d4fe729f06c7780b629fc197d4f5dd52ab7ea49b
Ryan Warrener <ryan.warrener@ontariotechu.net>
2025-09-20 09:24:22 -0400

    Completed Question #3019

---
+++ Python/#3000-3999/#3000-3099/#3019_Number_Of_Changing_Keys/Main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countKeyChanges(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(s) - 1):
+            if s[i].lower() != s[i + 1].lower():
+                count += 1
+        return count
