ca896e3c199a111f93d6625e0ab964f48b8701ef
Ryan Warrener <ryan.warrener@ontariotechu.net>
2025-11-14 14:53:58 -0500

    Started Maths Quest

---
+++ Python/Quest/Maths/Arithmetic_&_Basic_Reasoning/Q1.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        arr.sort(reverse=True)
+        for i in range(1, len(arr) - 1):
+            if abs(arr[i] - arr[i - 1]) != abs(arr[i + 1] - arr[i]):
+                return False
+        return True
+
